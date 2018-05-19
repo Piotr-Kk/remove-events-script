adb09085580a7ba8dbf272e9ed6eceb89107706c
PiotrKorczyk <p.k.6@wp.pl>
2018-05-19 18:07:14 +0200

    chnges to run script online

--- issue.py
+++ issue.py
@@ -6,23 +6,24 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 import datetime
 import json
-import sys
+import time
 
-CALENDAR_ID = 'xxx@gmail.com'
+
+CALENDAR_ID = 'piotr.korczyk66@gmail.com'
 EVENT_NAME = 'issue call'
 # to do: add requirment pip install, configure moto account
 
-SCOPES = 'https://www.googleapis.com/auth/calendar'
-store = file.Storage('credentials.json')
-creds = store.get()
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-    creds = tools.run_flow(flow, store)
-service = build('calendar', 'v3', http=creds.authorize(Http()))
-
+def buildServiceConnection():
+    SCOPES = 'https://www.googleapis.com/auth/calendar'
+    store = file.Storage('credentials.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('calendar', 'v3', http=creds.authorize(Http()))
+    return service
 
 def getLastEventDate(eventName):
-    lastEventName = eventName
     today = datetime.datetime.utcnow()
     pastThreeWeeks = today + datetime.timedelta(weeks=-3)
     pastThreeWeeks = pastThreeWeeks.isoformat() + 'Z'
@@ -43,8 +44,9 @@ def getLastEventDate(eventName):
     try:
         lastEvent = filtredEvents[len(filtredEvents)-1]
     except IndexError:
-        print('Cant find any '+eventName+' from past!')
-        sys.exit()
+        print('Cant find any '+eventName+' from last 3 weeks!')
+        return 0
+    print('Datetime of last '+EVENT_NAME+' (in local time): '+str(lastEvent['end']['dateTime']))
     return lastEvent['end']['dateTime'] # return date in LOCAL TIME, NO UTC !!! 
     
 
@@ -58,26 +60,33 @@ def findAndDeleteEvent(eventName, calendarService):
         today.year, today.month, today.day, 23,  59).isoformat() + 'Z'
     eventsList = service.events().list(calendarId=CALENDAR_ID,
                                        singleEvents=True, orderBy='startTime',
-                                       timeMin=now, timeMax=endOfDay).execute()
+                                       timeMin=now, timeMax=endOfDay, q= eventNameToDelete).execute()
     eventsListItems = eventsList['items']
     itemToDeleteId = ''
+    itemToDeleteDate = ''
     for item in eventsListItems:
         if item['summary'] == eventNameToDelete:
             itemToDeleteId = item['id']
+            itemToDeleteDate = item['start']['dateTime']
             print('ID: '+itemToDeleteId)
             break
     if itemToDeleteId == '':
-        print('Cant found event: '+eventNameToDelete +
-              '! Timerange(in UTC): from '+now+' to '+endOfDay+'\n')
-        sys.exit()
+        print('Cant find event: '+eventNameToDelete +
+              '! Time range(in UTC): from '+now+' to '+endOfDay+'')
     try:
-        deleteInfo = service.events().delete(calendarId=CALENDAR_ID,
+        service.events().delete(calendarId=CALENDAR_ID,
                                              eventId=itemToDeleteId).execute()
-        print('Event '+eventNameToDelete+' has been deleted')
+        print('Event '+eventNameToDelete+' has been deleted, evetn time in local time: ' +itemToDeleteDate)
     except:
-        print('Deleting went wrong!\n')
-        raise
+        print('Deleting went wrong!')
 
 
-# findAndDeleteEvent(EVENT_NAME, service)
-print(getLastEventDate(EVENT_NAME))
+while True:
+    t = datetime.datetime.utcnow()
+    print('----- Issue script ----- utc time: '+str(t))
+    service = buildServiceConnection()
+    getLastEventDate(EVENT_NAME)
+    findAndDeleteEvent(EVENT_NAME, service)
+    print('----- end of script -----\n')
+    
+    time.sleep(600)
